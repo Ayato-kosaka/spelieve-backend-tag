dcde40c3e7f40def062a68352a2177bfea9096db
Takapy1 <75763679+Takapy1@users.noreply.github.com>
2022-12-03 19:52:55 +0900

    Update src/HBL01MItineraryHashtag/HBL01MItineraryHashtag.controller.ts
    Co-authored-by: Ayato-kosaka <53715839+Ayato-kosaka@users.noreply.github.com>

--- src/HBL01MItineraryHashtag/HBL01MItineraryHashtag.controller.ts
+++ src/HBL01MItineraryHashtag/HBL01MItineraryHashtag.controller.ts
@@ -7,8 +7,9 @@ export class HBL001SetMItineraryTagDataController {
   constructor(private readonly service: HBL001SetMItineraryTagDataService) {}
 
   // curl -XPOST -H "Content-Type:application/json" localhost:3072/HBL001
-  @Get()
-  HBL01() {
+  @Post()
+  HBL01(@Body() HBL01Body: Record<string, never>) {
     return this.service.doExecute();
   }
+}
 }
