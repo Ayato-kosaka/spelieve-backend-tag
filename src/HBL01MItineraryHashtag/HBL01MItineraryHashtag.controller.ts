f5d4cbbd45aeb7bdd667adea59e748a33406ac6a
Takapy1 <75763679+Takapy1@users.noreply.github.com>
2022-12-03 19:50:46 +0900

    Update src/HBL01MItineraryHashtag/HBL01MItineraryHashtag.controller.ts
    Co-authored-by: Ayato-kosaka <53715839+Ayato-kosaka@users.noreply.github.com>

--- src/HBL01MItineraryHashtag/HBL01MItineraryHashtag.controller.ts
+++ src/HBL01MItineraryHashtag/HBL01MItineraryHashtag.controller.ts
@@ -6,7 +6,7 @@ import { HBL01MItineraryHashtagService } from './HBL01MItineraryHashtag.service'
 export class HBL01MItineraryHashtagController {
   constructor(private readonly service: HBL01MItineraryHashtagService) {}
 
-  // curl -XGET -H "Content-Type:application/json" localhost:3000/HBL01
+  // curl -XPOST -H "Content-Type:application/json" localhost:3072/HBL001
   @Get()
   HBL01() {
     return this.service.doExecute();
